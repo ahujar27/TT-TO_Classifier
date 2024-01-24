4ceabcac1c03612c858ac9a6fff090639a8b8d1d
Rohil Ahuja <41922715+ahujar27@users.noreply.github.com>
2024-01-23 22:02:29 -0600

    Create make.paired-fastq-to-unmapped-bam.input.json.py

---
+++ SNV_CNV/alignment/make.paired-fastq-to-unmapped-bam.input.json.py
@@ -0,0 +1,37 @@
+import pysam
+import json
+import sys
+
+sourceBam = sys.argv[1]
+fqRoot = sys.argv[2]
+outName = sys.argv[3]
+
+bam = pysam.AlignmentFile(sourceBam, mode='rb')
+bam_header = dict((k, v) for k, v in bam.header.items())
+if type(bam_header['RG'])==list:
+    dictRG = bam_header['RG'][0]
+else:
+    dictRG = bam_header['RG']
+
+tagDict = {}
+tags = ['SM','ID','LB','PU','DT','PL','CN']
+
+for tag in tags:
+    if tag in dictRG:
+        tagDict[tag] = dictRG[tag]
+    else:
+        tagDict[tag] = 'NA'
+
+jsonDict = {'ConvertPairedFastQsToUnmappedBamWf.sample_name':tagDict['SM'],
+            'ConvertPairedFastQsToUnmappedBamWf.readgroup_name':tagDict['ID'],
+            'ConvertPairedFastQsToUnmappedBamWf.fastq_1':fqRoot+'.1.fq.gz',
+            'ConvertPairedFastQsToUnmappedBamWf.fastq_2':fqRoot+'.2.fq.gz',
+            'ConvertPairedFastQsToUnmappedBamWf.library_name':tagDict['LB'],
+            'ConvertPairedFastQsToUnmappedBamWf.platform_unit':tagDict['PU'],
+            'ConvertPairedFastQsToUnmappedBamWf.run_date':tagDict['DT'],
+            'ConvertPairedFastQsToUnmappedBamWf.platform_name':tagDict['PL'],
+            'ConvertPairedFastQsToUnmappedBamWf.sequencing_center':tagDict['CN'],
+            'ConvertPairedFastQsToUnmappedBamWf.make_fofn':'true'}
+
+with open(outName, 'w') as fout:
+    json.dump(jsonDict, fout)
