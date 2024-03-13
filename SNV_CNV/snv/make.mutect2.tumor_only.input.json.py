e8d3e95619c6f5cbc4450ae826798546e579877a
Rohil Ahuja <41922715+ahujar27@users.noreply.github.com>
2024-03-13 13:46:18 -0500

    Update make.mutect2.tumor_only.input.json.py

--- SNV_CNV/snv/make.mutect2.tumor_only.input.json.py
+++ SNV_CNV/snv/make.mutect2.tumor_only.input.json.py
@@ -7,31 +7,29 @@ outName = sys.argv[3]
 intervalsPath = sys.argv[4]
 refPath = sys.argv[5]
 
-appPath = /opt/gatk-4.4.0.0/
-
 jsonDict = {
   "Mutect2.gatk_docker": "broadinstitute/gatk:4.1.4.1",
-  "Mutect2.gatk_override": "{}/gatk-package-4.1.4.1-local.jar".format(appPath),
+  "Mutect2.gatk_override": "/opt/gatk-4.4.0.0/gatk-package-4.1.4.1-local.jar".format(appPath),
 
   "Mutect2.intervals": intervalsPath,
   "Mutect2.scatter_count": 50,
   "Mutect2.m2_extra_args": "--downsampling-stride 20 --max-reads-per-alignment-start 6 --max-suspicious-reads-per-alignment-start 6",
 
-  "Mutect2.ref_fasta": "{}/Homo_sapiens_assembly38.fasta".format(refPath),
-  "Mutect2.ref_dict": "{}/Homo_sapiens_assembly38.dict".format(refPath),
-  "Mutect2.ref_fai": "{}/Homo_sapiens_assembly38.fasta.fai".format(refPath),
+  "Mutect2.ref_fasta": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta",
+  "Mutect2.ref_dict": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dict",
+  "Mutect2.ref_fai": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.fai",
 #  "Mutect2.normal_reads": normalBam,
 #  "Mutect2.normal_reads_index": normalBamInd,
   "Mutect2.tumor_reads": tumorBam,
   "Mutect2.tumor_reads_index": tumorBamInd,
 
-  "Mutect2.pon": "{}/somatic-hg38/1000g_pon.hg38.vcf".format(refPath),
-  "Mutect2.pon_idx": "{}/somatic-hg38/1000g_pon.hg38.vcf.idx".format(refPath),
-  "Mutect2.gnomad": "{}/somatic-hg38/af-only-gnomad.hg38.vcf".format(refPath),
-  "Mutect2.gnomad_idx": "{}/somatic-hg38/af-only-gnomad.hg38.vcf.idx".format(refPath),
-  "Mutect2.variants_for_contamination": "{}/somatic-hg38/small_exac_common_3.hg38.vcf".format(refPath),
-  "Mutect2.variants_for_contamination_idx": "{}/somatic-hg38/small_exac_common_3.hg38.vcf.idx".format(refPath),
-  "Mutect2.realignment_index_bundle": "{}/Homo_sapiens_assembly38.index_bundle".format(refPath)
+  "Mutect2.pon": "gs://gatk-best-practices/somatic-hg38/1000g_pon.hg38.vcf.gz",
+  "Mutect2.pon_idx": "gs://gatk-best-practices/somatic-hg38/1000g_pon.hg38.vcf.gz.tbi",
+  "Mutect2.gnomad": "gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz",
+  "Mutect2.gnomad_idx": "gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz.tbi",
+  "Mutect2.variants_for_contamination": "gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf.gz",
+  "Mutect2.variants_for_contamination_idx": "gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf.gz.tbi",
+  "Mutect2.realignment_index_bundle": "gs://gatk-test-data/mutect2/Homo_sapiens_assembly38.index_bundle"
 }
 
 with open(outName, 'w') as fout:
