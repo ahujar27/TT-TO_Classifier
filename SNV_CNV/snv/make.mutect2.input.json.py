84f02c8f0a2a879bb47aa3d4ad94fa18519291be
Rohil Ahuja <41922715+ahujar27@users.noreply.github.com>
2024-03-13 01:04:33 -0500

    Update make.mutect2.input.json.py

--- SNV_CNV/snv/make.mutect2.input.json.py
+++ SNV_CNV/snv/make.mutect2.input.json.py
@@ -19,21 +19,21 @@ jsonDict = {
   "Mutect2.scatter_count": 50,
   "Mutect2.m2_extra_args": "--downsampling-stride 20 --max-reads-per-alignment-start 6 --max-suspicious-reads-per-alignment-start 6",
 
-  "Mutect2.ref_fasta": "{}/Homo_sapiens_assembly38.fasta".format(refPath),
-  "Mutect2.ref_dict": "{}/Homo_sapiens_assembly38.dict".format(refPath),
-  "Mutect2.ref_fai": "{}/Homo_sapiens_assembly38.fasta.fai".format(refPath),
+  "Mutect2.ref_fasta": "{}/GRCh38.d1.vd1.fa".format(refPath),
+  "Mutect2.ref_dict": "{}/GRCh38.d1.vd1.fa.dict".format(refPath),
+  "Mutect2.ref_fai": "{}/GRCh38.d1.vd1.fa.fai".format(refPath),
   "Mutect2.normal_reads": normalBam,
   "Mutect2.normal_reads_index": normalBamInd,
   "Mutect2.tumor_reads": tumorBam,
   "Mutect2.tumor_reads_index": tumorBamInd,
 
-  "Mutect2.pon": "{}/somatic-hg38/1000g_pon.hg38.vcf".format(refPath),
-  "Mutect2.pon_idx": "{}/somatic-hg38/1000g_pon.hg38.vcf.idx".format(refPath),
-  "Mutect2.gnomad": "{}/somatic-hg38/af-only-gnomad.hg38.vcf".format(refPath),
-  "Mutect2.gnomad_idx": "{}/somatic-hg38/af-only-gnomad.hg38.vcf.idx".format(refPath),
-  "Mutect2.variants_for_contamination": "{}/somatic-hg38/small_exac_common_3.hg38.vcf".format(refPath),
-  "Mutect2.variants_for_contamination_idx": "{}/somatic-hg38/small_exac_common_3.hg38.vcf.idx".format(refPath),
-  "Mutect2.realignment_index_bundle": "{}/Homo_sapiens_assembly38.index_bundle".format(refPath)
+  "Mutect2.pon": "gs://gatk-best-practices/somatic-hg38/1000g_pon.hg38.vcf.gz".format(refPath),
+  "Mutect2.pon_idx": "gs://gatk-best-practices/somatic-hg38/1000g_pon.hg38.vcf.tbi".format(refPath),
+  "Mutect2.gnomad": "gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf".format(refPath),
+  "Mutect2.gnomad_idx": "gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.idx".format(refPath),
+  "Mutect2.variants_for_contamination": "gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf".format(refPath),
+  "Mutect2.variants_for_contamination_idx": "gs://gatk-best-practices/somatic-hg38/small_exac_common_3.hg38.vcf.idx".format(refPath),
+  "Mutect2.realignment_index_bundle": "Homo_sapiens_assembly38.index_bundle".format(refPath)
 }
 
 with open(outName, 'w') as fout:
