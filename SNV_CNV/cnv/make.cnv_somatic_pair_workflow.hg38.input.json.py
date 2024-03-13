d64f08f17bee39d1383e4882adcee2300a86d03b
Rohil Ahuja <41922715+ahujar27@users.noreply.github.com>
2024-03-13 14:17:30 -0500

    Update make.cnv_somatic_pair_workflow.hg38.input.json.py

--- SNV_CNV/cnv/make.cnv_somatic_pair_workflow.hg38.input.json.py
+++ SNV_CNV/cnv/make.cnv_somatic_pair_workflow.hg38.input.json.py
@@ -7,10 +7,7 @@ normalBam = sys.argv[3]
 normalBamInd = sys.argv[4]
 outName = sys.argv[5]
 intervalsPath = sys.argv[6]
-refPath = sys.argv[7]
-ponPath = sys.argv[8]
-
-appPath = /opt/gatk-4.4.0.0/
+ponPath = sys.argv[7]
 
 jsonDict = {
   "CNVSomaticPairWorkflow.tumor_bam": tumorBam,
@@ -18,15 +15,15 @@ jsonDict = {
   "CNVSomaticPairWorkflow.normal_bam": normalBam,
   "CNVSomaticPairWorkflow.normal_bam_idx": normalBamInd,
   
-  "CNVSomaticPairWorkflow.ref_fasta": "{}/GRCh38.d1.vd1.fa".format(refPath),
-  "CNVSomaticPairWorkflow.ref_fasta_fai": "{}/GRCh38.d1.vd1.fa.fai".format(refPath),
-  "CNVSomaticPairWorkflow.ref_fasta_dict": "{}/GRCh38.d1.vd1.fa.dict".format(refPath),
+  "CNVSomaticPairWorkflow.ref_fasta": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta",
+  "CNVSomaticPairWorkflow.ref_fasta_fai": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.fasta.fai",
+  "CNVSomaticPairWorkflow.ref_fasta_dict": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dict",
   "CNVSomaticPairWorkflow.common_sites": "gs://gatk-test-data/cnv/somatic/common_snps.interval_list",
   "CNVSomaticPairWorkflow.read_count_pon": ponPath, 
   "CNVSomaticPairWorkflow.intervals": intervalsPath,
 
   "CNVSomaticPairWorkflow.gatk_docker": "broadinstitute/gatk:4.4.0.0",
-  "CNVSomaticPairWorkflow.gatk4_jar_override": "{}/gatk-package-4.4.0.0-local.jar".format(appPath),
+  "CNVSomaticPairWorkflow.gatk4_jar_override": "/opt/gatk-4.4.0.0/gatk-package-4.4.0.0-local.jar",
 
   "CNVSomaticPairWorkflow.is_run_funcotator": "false"
 }
