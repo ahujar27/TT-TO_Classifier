ec5c602302d1a264dfd1ba31a2de228a6a7705bb
Rohil Ahuja <41922715+ahujar27@users.noreply.github.com>
2024-03-14 13:37:41 -0500

    update for gatk 4.1.4.1

--- SNV_CNV/cnv/make.cnv_somatic_panel_workflow.input.json.py
+++ SNV_CNV/cnv/make.cnv_somatic_panel_workflow.input.json.py
@@ -20,8 +20,8 @@ jsonDict = {
     "CNVSomaticPanelWorkflow.ref_fasta_dict": "gs://genomics-public-data/resources/broad/hg38/v0/Homo_sapiens_assembly38.dict",
     "CNVSomaticPanelWorkflow.intervals": intervalPath,
 
-    "CNVSomaticPanelWorkflow.gatk_docker": "broadinstitute/gatk:4.4.0.0",
-    "CNVSomaticPanelWorkflow.gatk4_jar_override": "/opt/gatk-4.4.0.0/gatk-package-4.4.0.0-local.jar".format(appPath),
+    "CNVSomaticPanelWorkflow.gatk_docker": "broadinstitute/gatk:4.1.4.1",
+    "CNVSomaticPanelWorkflow.gatk4_jar_override": "/opt/gatk-4.1.4.1/gatk-package-4.4.0.0-local.jar".format(appPath),
     "CNVSomaticPanelWorkflow.preemptible_attempts": "3"
 }
 
